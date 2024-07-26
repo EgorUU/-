08719ef89757b201ee46596fb74cb0e6f37d9362
EgorUU <egorsairanov12@gmail.com>
2024-07-27 00:05:57 +0300

    first

---
+++ script.js
@@ -0,0 +1,42 @@
+const block = document.querySelector('.target')
+const difficultyLevel = document.querySelectorAll('.wrapper-buttons>button')
+const wrapper = document.querySelector('.wrapper')
+let time
+difficultyLevel.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        switch (btn.textContent) {
+            case 'Легкий': 
+                time = 900
+                break;
+            case 'Средний': 
+                time = 600
+                break;
+            case 'Сложный': 
+                time = 400
+                break;
+        }
+        wrapper.style.display = 'none'
+        block.style.display = 'block'
+
+        // Интервал
+
+        const interval = setInterval(() => {
+            const randomWidth =  Math.floor(Math.random() * ((window.innerWidth - 50) - 1 + 1)) + 1
+            const randomHeight =  Math.floor(Math.random() * (window.innerHeight - 1 + 1)) + 1
+            block.style.top = `${randomHeight}px`
+            block.style.left = `${randomWidth}px`
+        }, time)
+
+        block.addEventListener('click', () => {
+            clearInterval(interval)
+            console.log('Игра завершена')
+            block.style.backgroundColor = 'black'
+        })
+
+        //
+    })
+})
+
+
+
+
